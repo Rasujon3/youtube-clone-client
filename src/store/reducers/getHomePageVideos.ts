f72ada6747895818266121dad2daeaaedd7a80d5
Rasujon3 <rasujon3@gmail.com>
2023-01-02 22:17:56 +0600

    cache problem solve done

--- src/store/reducers/getHomePageVideos.ts
+++ src/store/reducers/getHomePageVideos.ts
@@ -1,14 +1,14 @@
-import { YOUTUBE_API_URL } from "./../../utils/constants";
-import { RootState } from "./../index";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { RootState } from "..";
 import { HomePageVideos } from "../../Types";
 import { parseData } from "../../utils";
+import { YOUTUBE_API_URL } from "../../utils/constants";
 
-const API_key = process.env.REACT_APP_YOUTUBE_DATA_API_KEY;
+const API_KEY = process.env.REACT_APP_YOUTUBE_DATA_API_KEY;
 
 export const getHomePageVideos = createAsyncThunk(
-  "youtubeApp/homePageVidos",
+  "youtubeApp/homePageVidoes",
   async (isNext: boolean, { getState }) => {
     const {
       youtubeApp: { nextPageToken: nextPageTokenFromState, videos },
@@ -16,11 +16,11 @@ export const getHomePageVideos = createAsyncThunk(
     const {
       data: { items, nextPageToken },
     } = await axios.get(
-      `${YOUTUBE_API_URL}/search?maxResults=20&q="reactjs projects"&key=${API_key}&part=snippet&type=video&${
+      `${YOUTUBE_API_URL}/search?maxResults=20&q="reactjs projects"&key=${API_KEY}&part=snippet&type=video&${
         isNext ? `pageToken=${nextPageTokenFromState}` : ""
       }`
     );
-    console.log({ items, nextPageTokenFromState, nextPageToken });
+    // console.log({ items, nextPageTokenFromState, nextPageToken });
     const parsedData: HomePageVideos[] = await parseData(items);
     return { parsedData: [...videos, ...parsedData], nextPageToken };
   }
